177f9844f0f33434951956f1361a863a1ba59476
Clayton Miller <cbmiller4897@gmail.com>
2021-09-20 01:18:41 -0500

    added code from yt tutorial. Search button click stores city name in local storage and creates button in search History

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,72 +1,115 @@
 "api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}"
 
 // Variables
-var apiFiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=56ef42523d8ac74ceb13ce7f908fa8e6;
-// var apiOneCall = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=" + part + "&appid=" + apiKey;
+// const apiOneCall = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=" + part + "&appid=56ef42523d8ac74ceb13ce7f908fa8e6";
 
 // Query Selecors
-var cityName = document.querySelector("#cityName");
-var searchBtn = document.querySelector("#searchBtn");
+const cityName = document.querySelector("#cityName");
+const searchBtn = document.querySelector("#searchBtn");
+const searchHistory = document.querySelector("#searchHistory");
+
+const apiFiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName.value + "&appid=56ef42523d8ac74ceb13ce7f908fa8e6";
+
+const cityInput = cityName["name"]
+
+const allCities = JSON.parse(localStorage.getItem("allCities")) || [];
 
 // Functions
 
-var search = function() {
-        console.log("search function is working");
+const addCity = (name) => {
+    allCities.push({
+        name
+    });
+
+    localStorage.setItem("allCities", JSON.stringify(allCities));
+
+    return {name};
+};
+const createSearchHistoryBtn = ({name}) => {
+    // create elements
+    const cityBtn = document.createElement("button");
+    cityBtn.classList.add("searchHistory-append", "col-sm-10");
+
+    // Fill the content
+    cityBtn.innerHTML = name;
+
+    // Add to the DOM
+    searchHistory.appendChild(cityBtn);
+};
+
+allCities.forEach(createSearchHistoryBtn);
+
+searchBtn.onclick = e => {
+
+    e.preventDefault();
+    console.log("the onclick function for searchBtn is working!")
+
+    const newCity = addCity(
+        cityName.value
+    );
+
+    createSearchHistoryBtn(newCity);
+
+    cityName.value = "";
+
+    console.log();
+
+};
+
+
+
+
+// Old code ------------------------------------------------------------------------
+
+// var search = function() {
+//         console.log("search function is working");
     
-    var city = $(this).siblings('textarea').val().trim();
-        console.log(city);
-        console.log(apiFiveDay);
+//     var city = $(this).siblings('textarea').val().trim();
+//         console.log(city);
+//         console.log(apiFiveDay);
 
-    localStore(city);
-    createHistoryBtn(city);
-};
+//     localStore(city);
+//     createHistoryBtn(city);
+// };
 
-// Save data to local storage
-var localStore = function(city){
-    window.localStorage.setItem('cityName', city);
-};
+// var createHistoryBtn = function(city) {
+//     $.each(city, function(list, item) {
+//         $('#search-history').append("<button id='appendBtn' class='search-history-append col-sm-10' >" + city + "</button>");
 
-var createHistoryBtn = function(city) {
-    $.each(city, function(list, item) {
-        $('#search-history').append("<button id='appendBtn' class='search-history-append col-sm-10' >" + city + "</button>");
+//     })
+// };
 
-    })
-};
+// var fetchData = function(cityName){
 
-var fetchData = function(cityName){
-
-    // alert("This is the fetchData function " + cityName);
+//     console.log("This is the fetchData function " + cityName);
 
 
-    fetch(apiFiveDay).then(function(response){
+//     fetch(apiFiveDay).then(function(response){
 
-        console.log(response.json());
+//         console.log(response.json());
 
+//         if (response.ok) {
+//             console.log(response);
+//             response.json().then(function(data){
+//                 console.log(data);
+//             })
+//         } else {
+//             alert("Error " + response.statusText);
+//         }
 
-        // if (response.ok) {
-        //     console.log(response);
-        //     response.json().then(function(data){
-        //         console.log(data);
-        //     })
-        // } else {
-        //     alert("Error " + response.statusText);
-        // }
+//     })
 
-    })
+//     console.log(fetch(apiFiveDay));
 
-console.log(fetch(apiFiveDay));
+// };
 
-};
+// var loadData = function() {
+//     var name = JSON.parse(localStorage.getItem("cityName"));
 
-var loadData = function() {
-    var name = JSON.parse(localStorage.getItem("cityName"));
+//     $.each(name, function(list, item){
+//         createHistoryBtn(item);
+//         console.log(item);
+//     })
+// };
 
-    $.each(name, function(list, item){
-        createHistoryBtn(item);
-        console.log(item);
-    })
-};
-
-$('#search-btn').on('click', search);
-
-loadData();
+// $('#search-btn').on('click', search);
